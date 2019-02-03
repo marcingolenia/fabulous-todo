6a9b764b98b536442264cc8a07a2d22ea4731305
Marcin Golenia <marcingolenia@gmail.com>
2019-02-03 16:51:42 +0100

    initial commit - runnable app with one dumb button

---
+++ ToDo/ToDo.fs
@@ -0,0 +1,82 @@
+﻿namespace ToDo
+
+open Fabulous.Core
+open Fabulous.DynamicViews
+open Xamarin.Forms
+
+module App = 
+    type Todo = { Title: string; Description: string; }
+    type Model = { ToDos: Todo list }
+    type Msg = 
+        | AddToDo 
+        | RemoveTodo 
+        | CompleteTodo
+
+    let initModel = { ToDos = List.Empty }
+    let init () = initModel, Cmd.none
+    let update msg model =
+        match msg with
+        | AddToDo -> { model with ToDos = {Title="Title"; Description="Description"} :: model.ToDos }, Cmd.none
+
+    let view (model: Model) dispatch =
+        View.ContentPage(
+          content = View.StackLayout(padding = 10.0, verticalOptions = LayoutOptions.Center,
+            children = [ 
+                View.Label(text = "ToDo App", horizontalOptions = LayoutOptions.Center, widthRequest=200.0, horizontalTextAlignment=TextAlignment.Center);
+                View.Label(text = sprintf "%d" model.ToDos.Length, horizontalOptions = LayoutOptions.Center, widthRequest=200.0, horizontalTextAlignment=TextAlignment.Center)
+                View.Button(text = "Add", command = (fun () -> dispatch AddToDo), horizontalOptions = LayoutOptions.Start)
+            ]))
+
+    // Note, this declaration is needed if you enable LiveUpdate
+    let program = Program.mkProgram init update view
+
+type App () as app = 
+    inherit Application ()
+
+    let runner = 
+        App.program
+#if DEBUG
+        |> Program.withConsoleTrace
+#endif
+        |> Program.runWithDynamicView app
+
+#if DEBUG
+    // Uncomment this line to enable live update in debug mode. 
+    // See https://fsprojects.github.io/Fabulous/tools.html for further  instructions.
+    //
+    do runner.EnableLiveUpdate()
+#endif    
+
+    // Uncomment this code to save the application state to app.Properties using Newtonsoft.Json
+    // See https://fsprojects.github.io/Fabulous/models.html for further  instructions.
+#if APPSAVE
+    let modelId = "model"
+    override __.OnSleep() = 
+
+        let json = Newtonsoft.Json.JsonConvert.SerializeObject(runner.CurrentModel)
+        Console.WriteLine("OnSleep: saving model into app.Properties, json = {0}", json)
+
+        app.Properties.[modelId] <- json
+
+    override __.OnResume() = 
+        Console.WriteLine "OnResume: checking for model in app.Properties"
+        try 
+            match app.Properties.TryGetValue modelId with
+            | true, (:? string as json) -> 
+
+                Console.WriteLine("OnResume: restoring model from app.Properties, json = {0}", json)
+                let model = Newtonsoft.Json.JsonConvert.DeserializeObject<App.Model>(json)
+
+                Console.WriteLine("OnResume: restoring model from app.Properties, model = {0}", (sprintf "%0A" model))
+                runner.SetCurrentModel (model, Cmd.none)
+
+            | _ -> ()
+        with ex -> 
+            App.program.onError("Error while restoring model found in app.Properties", ex)
+
+    override this.OnStart() = 
+        Console.WriteLine "OnStart: using same logic as OnResume()"
+        this.OnResume()
+#endif
+
+
