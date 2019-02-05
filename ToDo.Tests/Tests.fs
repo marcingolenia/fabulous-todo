0b0b6f9b391872a5e6d3f0c0f150e37958d0c088
Marcin Golenia <marcingolenia@gmail.com>
2019-02-05 22:54:44 +0100

    add last test to update model function

--- ToDo.Tests/Tests.fs
+++ ToDo.Tests/Tests.fs
@@ -22,6 +22,32 @@ let ``After new ToDo is Added to list next new ToDo has empty title`` () =
     updatedModel.NewToDo.Title |> should equal ""
 
 [<Fact>]
-let ``Changing new ToDo title updates new Todo model`` () =
+let ``Changing new ToDo title in ui updates new Todo title in model`` () =
     let updatedModel = update (ToDoTitleChanged ("", "NewTitle")) initialModel |> fst
     updatedModel.NewToDo.Title |> should equal "NewTitle"
+
+[<Fact>]
+let ``ClearCompletedToDos does nothing if all todos are not completed`` () =
+    let model = { initialModel with ToDos = [{ Title = "1"; IsCompleted = false }; 
+                                             { Title = "2"; IsCompleted = false };]}
+    let updatedModel = update ClearCompletedToDos model |> fst
+    updatedModel.ToDos |> should equal model.ToDos
+
+[<Fact>]
+let ``ClearCompletedToDos Removes completed Todos from model`` () =
+    let model = { initialModel with ToDos = [{ Title = "1"; IsCompleted = false };
+                                             { Title = "2"; IsCompleted = true };
+                                             { Title = "3"; IsCompleted = true };]}
+    let updatedModel = update ClearCompletedToDos model |> fst
+    updatedModel.ToDos |> List.exactlyOne
+
+[<Fact>]
+let ``Sort ToDos moves completed ToDos to the end of the ToDo list`` () =
+    let firstCompletedItem = { Title = "1"; IsCompleted = true }
+    let secondNotCompletedItem = { Title = "2"; IsCompleted = false }
+    let thirdCompletedItem = { Title = "3"; IsCompleted = true }
+    let model = { initialModel with ToDos = [ firstCompletedItem; secondNotCompletedItem; thirdCompletedItem ]}
+    let updatedModel = update SortToDos model |> fst
+    updatedModel.ToDos |> List.head |> should equal secondNotCompletedItem
+    updatedModel.ToDos.[1] |> should equal firstCompletedItem
+    updatedModel.ToDos |> List.last |> should equal thirdCompletedItem
